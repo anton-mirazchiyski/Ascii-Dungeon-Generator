63a95f2ea52723f3b3803227369db2bd3f869397
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-03-22 16:59:16 +0200

    A function to determine the dungeon's entrance randomly
    The entrance could be on all possible sides and can be different every time

--- dungeons/dungeon.py
+++ dungeons/dungeon.py
@@ -1,13 +1,37 @@
+import random
+
 size = 10
 
 def build_dungeon_without_rooms():
     walls = '#' * size
-    roomless_dungeon = [walls for _ in range(size)]
+    roomless_dungeon = [list(walls) for _ in range(size)]
 
     return roomless_dungeon
 
 
+def determine_start_position(dungeon):
+    start_idx, end_index = 0, (size - 1)
+    possible_start_sides = ('top', 'left', 'bottom', 'right')
+
+    random_start_side = random.choice(possible_start_sides)
+    random_coordinates = None
+
+    match random_start_side:
+        case 'top':
+            random_coordinates = (start_idx, random.randint(start_idx, end_index))
+        case 'bottom':
+            random_coordinates = (end_index, random.randint(start_idx, end_index))
+        case 'left':
+            random_coordinates = (random.randint(start_idx, end_index), start_idx)
+        case 'right':
+            random_coordinates = (random.randint(start_idx, end_index), end_index)
+
+    start_row, start_col = random_coordinates
+    dungeon[start_row][start_col] = '.'
+
+
 dungeon = build_dungeon_without_rooms()
+determine_start_position(dungeon)
 
 for row in dungeon:
     print(*row, sep='')
