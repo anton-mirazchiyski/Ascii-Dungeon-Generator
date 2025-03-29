7a786532ec42acd8e662906633bef5c39d24b5ef
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-03-29 20:29:33 +0200

    Add function to collect random room cells
    Start implementing corridors/paths generation

--- dungeons/dungeon.py
+++ dungeons/dungeon.py
@@ -35,6 +35,8 @@ def determine_start_position(dungeon):
     start_row, start_col = random_coordinates
     dungeon[start_row][start_col] = Fore.YELLOW + '.' + Style.RESET_ALL
 
+    return start_row, start_col
+
 
 def position_is_available(row, col, dungeon):
     # a list of unavailable coordinates that keep the rooms spaced out
@@ -52,7 +54,7 @@ def position_is_available(row, col, dungeon):
     return True
 
 
-def mark_cell(dungeon, row, col):
+def mark_room_cell(dungeon, row, col):
     dungeon[row][col] = Fore.BLUE + 'r' + Style.RESET_ALL
 
 
@@ -62,26 +64,49 @@ def generate_random_room_position():
 
 
 def create_room(row, col, dungeon):
-    mark_cell(dungeon, row, col + 1)
-    mark_cell(dungeon, row + 1, col + 1)
-    mark_cell(dungeon, row + 1, col)
-    mark_cell(dungeon, row, col)
+    mark_room_cell(dungeon, row, col + 1)
+    mark_room_cell(dungeon, row + 1, col + 1)
+    mark_room_cell(dungeon, row + 1, col)
+    mark_room_cell(dungeon, row, col)
+
+
+def pick_random_room_cell(row, col):
+    random_cell_coordinates = random.choice([(row, col + 1), (row + 1, col + 1), (row + 1, col), (row, col)])
+    return random_cell_coordinates
 
 
 def generate_rooms(dungeon):
     possible_number_of_rooms = random.randint(2, 7)
+    random_room_cells = []
 
     for _ in range(possible_number_of_rooms):
         random_room_position = generate_random_room_position()
         current_row, current_col = random_room_position
         if position_is_available(current_row, current_col, dungeon):
             create_room(current_row, current_col, dungeon)
+            random_cell = pick_random_room_cell(current_row, current_col)
+            random_room_cells.append(random_cell)
+
+    return random_room_cells
+
+
+def mark_corridor_cell(dungeon, row, col):
+    dungeon[row][col] = '.'
+
+
+def is_out_of_bounds(row, col):
+    return row >= end_index or row <= start_idx or col >= end_index or col <= start_idx
+
+
+def generate_corridors(dungeon, row, col):
+    pass
 
 
 dungeon = build_dungeon_without_rooms()
-determine_start_position(dungeon)
-generate_rooms(dungeon)
+row_start, col_start = determine_start_position(dungeon)
+rooms_coordinates = generate_rooms(dungeon)
+generate_corridors(dungeon, row_start, col_start)
 
 print()
 for row in dungeon:
-    print(*row, sep='')
+    print(*row, sep=' ')
